89c8bf02d028c2cfdf54b420fff5e670214f05a6
Oleksander <123253746+Alex-diver@users.noreply.github.com>
2023-07-09 12:17:24 +0300

    first attempt

---
+++ src/index.js
@@ -0,0 +1,97 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { getImages } from './gallery';
+import { markupGellery } from './markup';
+
+const formEl = document.querySelector('#search-form');
+const inputEl = document.querySelector('#search-form input');
+const galleryItemsEl = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+let nameImages = '';
+let currentPage = 1;
+let totalPage = 0;
+let perPage = 0;
+
+export { nameImages, currentPage };
+
+formEl.addEventListener('submit', onSubmit);
+loadMoreBtn.addEventListener('click', onClickLoadMoreBtn);
+
+const galleryLightBox = new SimpleLightbox('.gallery a');
+
+async function onSubmit(evt) {
+  evt.preventDefault();
+  const {
+    elements: { searchQuery },
+  } = evt.currentTarget;
+
+  nameImages = searchQuery.value.trim();
+  currentPage = 1;
+  loadMoreBtn.hidden = true;
+
+  if (nameImages === '') {
+    evt.currentTarget.reset();
+    return;
+  }
+
+  try {
+    const dataGallery = await getImages();
+    galleryItemsEl.innerHTML = markupGellery(dataGallery.data.hits);
+    galleryLightBox.refresh();
+    if (dataGallery.data.hits.length) {
+      Notify.success(`Hooray! We found ${dataGallery.data.totalHits} images.`);
+    } else {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      galleryItemsEl.innerHTML = '';
+      loadMoreBtn.hidden = true;
+    }
+
+    totalPage = Math.ceil(
+      dataGallery.data.totalHits / dataGallery.data.hits.length
+    );
+    perPage = dataGallery.data.hits.length;
+
+    if (totalPage > currentPage) {
+      loadMoreBtn.hidden = false;
+    }
+  } catch (error) {
+    console.error(error);
+    galleryItemsEl.innerHTML = '';
+    loadMoreBtn.hidden = true;
+    currentPage = 1;
+  }
+}
+
+inputEl.addEventListener('input', event => {
+  if (event.currentTarget.value === '') {
+    galleryItemsEl.innerHTML = '';
+    loadMoreBtn.hidden = true;
+    currentPage = 1;
+  }
+});
+
+async function onClickLoadMoreBtn() {
+  currentPage += 1;
+  if (currentPage === totalPage) {
+    loadMoreBtn.hidden = true;
+
+    Notify.failure('Were sorry, but youve reached the end of search results.');
+  }
+  try {
+    const dataGalleryPagination = await getImages();
+    galleryItemsEl.insertAdjacentHTML(
+      'beforeend',
+      markupGellery(dataGalleryPagination.data.hits)
+    );
+    galleryLightBox.refresh();
+    perPage = dataGalleryPagination.data.hits.length;
+  } catch (error) {
+    console.error(error);
+    galleryItemsEl.innerHTML = '';
+    loadMoreBtn.hidden = true;
+    currentPage = 1;
+  }
+}
